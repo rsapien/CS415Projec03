0ce6b6a46ece1da66bed92bacefeba2e863799a8
rsapien <sapien@sonoma.edu>
2020-04-26 17:55:58 -0700

    - started main() - needs to be run from the command line for it to work --> python Main.py '#' - grabs needed files from specified directory - inputs files into a list ('fileList')

--- Main.py
+++ Main.py
@@ -1,4 +1,27 @@
-
-
+import os
+import sys
 
 def main():
+    args = sys.argv
+    print(len(args))
+    if len(args) != 3:
+        print("Invalid number of arguments. Exiting...")
+        return
+
+    dir = args[1]
+    num = args[2]
+    files = os.listdir(dir)
+    cFile = "p0" + num + "_c.txt"
+    wFile = "p0" + num + "_w.txt"
+    vFile = "p0" + num + "_v.txt"
+    fileList = []
+    for file in files:
+        if cFile == file:
+            fileList.append(file)
+        elif wFile == file:
+            fileList.append(file)
+        elif vFile == file:
+            fileList.append(file)
+    print(fileList)
+
+main()
