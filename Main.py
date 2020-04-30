b563e4b1eae3f1680b07cb1a11d10e79a5030426
rsapien <sapien@sonoma.edu>
2020-04-30 14:01:21 -0700

    - finished first two print statements - README edited to successfully show how to run the program - all files are inserted into lists with corresponding letter variable names

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 import os
 import sys
 
-#def dynamic(capacity, weights[], values[]):
+def dynamic(capacity, weights, values):
 
+    return
 
 def main():
     args = sys.argv
@@ -14,23 +15,46 @@ def main():
     dir = args[1]
     num = args[2]
     files = os.listdir(dir)
-    cFile = "p0" + num + "_c.txt"
-    wFile = "p0" + num + "_w.txt"
-    vFile = "p0" + num + "_v.txt"
+    cFileName = "p" + num + "_c.txt"
+    wFileName = "p" + num + "_w.txt"
+    vFileName = "p" + num + "_v.txt"
     fileList = []
     for file in files:
-        if cFile == file:
+        if cFileName == file:
             fileList.append(file)
-        elif wFile == file:
+        elif wFileName == file:
             fileList.append(file)
-        elif vFile == file:
+        elif vFileName == file:
             fileList.append(file)
 
+    c = []
+    w = []
+    v = []
+
+    for file in fileList:
+        FDir = dir + '/' + file
+        #print(FDir, type(FDir))
+        F = open(FDir, 'r')
+
+        if file[4] == 'c':
+            c = F.read().splitlines()
+            F.close()
+
+        elif file[4] == 'w':
+            w = F.read().splitlines()
+            F.close()
+            for i in range(len(w)):
+                w[i] = w[i].strip(" ")
+
+        elif file[4] == 'v':
+            v = F.read().splitlines()
+            for i in range(len(v)):
+                v[i] = v[i].strip(" ")
+            F.close()
+
     print("File containing the capacity, weights, and values are: ", end=" ")
-    for i in range(len(fileList)):
-        if i == len(fileList)-1:
-            print(fileList[i])
-        else:
-            print(fileList[i], end= ", ")
+    print(cFileName, wFileName, "", sep=", ", end=" "); print(vFileName)
+
+    print("Knapsack capacity = {0}. Total number of items = {1}".format(c[0], len(w)))
 
 main()
